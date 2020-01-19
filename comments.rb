180c28cd6edcfee6fa88bade36aa7f382261bfee
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-19 17:38:10 +0000

    Automatically backed up by Learn

---
+++ comments.rb
@@ -0,0 +1,2 @@
+Here we have a line that is visible
+#Here we have a line that is commented out
---
+++ greeting.rb
@@ -0,0 +1,21 @@
+def greeting
+  puts "Hello World"
+end
+ 
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+ 
+say_greeting_five_times
+
+
+new_hash = {
+  created: Time.now,
+  message: "Hello world!"
+}
+
+new_hash
